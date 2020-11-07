7f80c3a171bfc37a2a5a11042d99706ac1dbc394
Przemek <przemyslawdopart@gmail.com>
2020-11-07 14:08:07 +0100

    first commit

---
+++ src/test/java/com/example/inqoo/springbootcamp/hibernate/exercise1/Exercise1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.inqoo.springbootcamp.hibernate.exercise1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Exercise1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
